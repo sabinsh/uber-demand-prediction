112a4cd99550306f1b39b61105fd52a1fda7390d
Himanshu Arora <129079320+Himanshu-1703@users.noreply.github.com>
2025-03-03 12:42:18 +0530

    added mlflow tracking and model registry to pipeline

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,3 +1,6 @@
+import mlflow
+import dagshub
+import json
 import pandas as pd
 import joblib
 from pathlib import Path
@@ -5,6 +8,15 @@ import logging
 from sklearn import set_config
 from sklearn.metrics import mean_absolute_percentage_error
 
+
+import dagshub
+dagshub.init(repo_owner='himanshu1703', repo_name='uber-demand-prediction', mlflow=True)
+
+# set the mlflow tracking uri
+mlflow.set_tracking_uri("https://dagshub.com/himanshu1703/uber-demand-prediction.mlflow")
+
+# set the experiment name
+mlflow.set_experiment("DVC Pipeline")
 
 set_config(transform_output="pandas")
 
@@ -26,16 +38,27 @@ def load_model(model_path):
     return model
 
 
+def save_run_information(run_id, artifact_path, model_uri, path):
+    run_information = {
+        "run_id": run_id,
+        "artifact_path": artifact_path,
+        "model_uri": model_uri
+    }
+    with open(path, "w") as f:
+        json.dump(run_information, f, indent=4)
+
+
 if __name__ == "__main__":
     # current path
     current_path = Path(__file__)
     # set the root path
     root_path = current_path.parent.parent.parent
     # data_path
-    data_path = root_path / "data/processed/test.csv"
+    train_data_path = root_path / "data/processed/train.csv"
+    test_data_path = root_path / "data/processed/test.csv"
     
     # read the data
-    df = pd.read_csv(data_path, parse_dates=["tpep_pickup_datetime"])
+    df = pd.read_csv(test_data_path, parse_dates=["tpep_pickup_datetime"])
     logger.info("Data read successfully")
     
     # set the datetime column as index
@@ -65,4 +88,45 @@ if __name__ == "__main__":
     # calculate the loss
     loss = mean_absolute_percentage_error(y_test, y_pred)
     logger.info(f"Loss: {loss}")
+    
+    # mlflow tracking
+    with mlflow.start_run(run_name="model"):    
+        # log the model parameters
+        mlflow.log_params(model.get_params())
+        
+        # log the mertic
+        mlflow.log_metric("MAPE", loss)
+        
+        # converts the datasets into mlfow datasets
+        training_data = mlflow.data.from_pandas(pd.read_csv(train_data_path, parse_dates=["tpep_pickup_datetime"]).set_index("tpep_pickup_datetime"), targets="total_pickups")
+        
+        validation_data = mlflow.data.from_pandas(pd.read_csv(test_data_path, parse_dates=["tpep_pickup_datetime"]).set_index("tpep_pickup_datetime"), targets="total_pickups")
+        
+        # log the datasets
+        mlflow.log_input(training_data, "training")
+        mlflow.log_input(validation_data, "validation")
+        
+        # model signature
+        model_signature = mlflow.models.infer_signature(X_test_encoded, y_pred)
+        
+        # log sklearn model
+        logged_model = mlflow.sklearn.log_model(model, "demand_prediction", 
+                                 signature=model_signature,
+                                 pip_requirements="requirements.txt")
+        
+        
+    # get the run id and arifact uri
+    run_id = logged_model.run_id
+    artifact_path = logged_model.artifact_path
+    model_uri = logged_model.model_uri 
+    logger.info("Mlflow logging complete")
+    
+    # save to json file
+    json_file_save_path = root_path / "run_information.json"
+    save_run_information(run_id=run_id,
+                         artifact_path=artifact_path,
+                         model_uri=model_uri,
+                         path=json_file_save_path)
+    logger.info("Run information saved successfully")
+    
     
